2267674d559b4ddcc6e6d98fc88178446f8cbd66
Mollie <mollie.stein@gmail.com>
2017-11-22 11:51:56 -0500

    added navigation bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import Game from './containers/Game';
+import NavBar from './components/NavBar';
+import Scores from './components/Scores';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 export default class App extends Component {
-  
-  render() {
-    return (
-      <div>
-        <Game />
-     </div>
-    );
-  }
+
+render() {
+	return (
+		<Router>
+			<div>
+				<NavBar />
+				<Route exact path="/" component={Game} />
+				<Route exact path="/scores" component={Scores} />
+			</div>
+		</Router>
+		);
+}
 }
 
---
+++ client/src/components/Scores.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Scores = (props) => {
+
+return "scores"
+
+}
+
+export default Scores
