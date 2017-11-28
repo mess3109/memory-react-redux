f17d040216b1936de01ce81af4a92b7975662b8f
Mollie <mollie.stein@gmail.com>
2017-11-28 14:40:27 -0500

    removed serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store'
 require('dotenv').config()
@@ -13,4 +13,3 @@ ReactDOM.render(
   </Provider>
   , document.getElementById('root')
 );
-registerServiceWorker();
