faf1d2236b44d0f2cd819384a659ee90e9006685
Mollie <mollie.stein@gmail.com>
2017-11-30 11:24:52 -0500

    Adding one more backup image

--- client/src/reducers/gameReducer.js
+++ client/src/reducers/gameReducer.js
@@ -1,5 +1,5 @@
 let cards = []
-let backupImages = ['https://d32dm0rphc51dk.cloudfront.net/sLk9dUSR9rK8VHKj8XwmiA/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/nYda7YCiY06VYGVRvfgc4A/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/YlIcfcosMKSItnQxsXpW6w/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/cM62_1h4Szq9gFkLWti3og/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/tAEiY881HVs_33crt6Swng/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/mdk-UfAfmfx3axsi9IGBcg/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/ldMX40edBZskPD-KF9015w/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/i4Dkd3mp60XpKwoplsEUGA/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/i4Dkd3mp60XpKwoplsEUGA/medium.jpg','https://d32dm0rphc51dk.cloudfront.net/z2fgbNgHsEsrUTe0dR4g6g/medium.jpg']
+let backupImages = ['https://d32dm0rphc51dk.cloudfront.net/sLk9dUSR9rK8VHKj8XwmiA/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/nYda7YCiY06VYGVRvfgc4A/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/YlIcfcosMKSItnQxsXpW6w/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/cM62_1h4Szq9gFkLWti3og/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/tAEiY881HVs_33crt6Swng/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/mdk-UfAfmfx3axsi9IGBcg/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/ldMX40edBZskPD-KF9015w/medium.jpg', 'https://d32dm0rphc51dk.cloudfront.net/i4Dkd3mp60XpKwoplsEUGA/medium.jpg','https://d32dm0rphc51dk.cloudfront.net/z2fgbNgHsEsrUTe0dR4g6g/medium.jpg','https://d32dm0rphc51dk.cloudfront.net/AeHNi1Yc3GZ9gET1vlxGxg/medium.jpg']
 
 
 function shuffle(array) {
@@ -32,10 +32,10 @@ export default function game(state = initialState, action) {
   switch(action.type) {
     case 'START':
 
-    if (!action.payload[0]) {
+    if (action.payload[0].length > 0) {
       cardsOrig = action.payload.map((image) => { return image._links.thumbnail.href })
     } else {
-    cardsOrig = backupImages.map((element) => element)
+      cardsOrig = backupImages.map((element) => element)
     }
 
     cardsOrig = cardsOrig.slice(0,10)
