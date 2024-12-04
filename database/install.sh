63a0ca7913b5c201b22af5f23abd03ae00b75e19
mollie stein <molliestein@mollies-Mac-mini.local>
2024-12-03 21:41:42 -0500

    Clean up scripts.

--- database/install.sh
+++
@@ -1,3 +0,0 @@
-mysql --host=127.0.0.1 --user=root --password=root -e "CREATE DATABASE IF NOT EXISTS memory;"
-mysql --host=127.0.0.1 --user=root --password=root --database=memory < ./db/schema.sql
-mysql --host=127.0.0.1 --user=root --password=root --database=memory < ./db/knex_migrations.sql
